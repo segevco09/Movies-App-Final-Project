250346a3e11bbc17996a5200ee53486c1d638c50
myName <tou@myemail.com>
2025-02-16 15:21:58 +0200

    editing single movie fragment and xml

--- app/src/main/java/com/example/movieapp/ui/movieDetail/MovieDetailFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/movieDetail/MovieDetailFragment.kt
@@ -1,24 +1,52 @@
 package com.example.movieapp.ui.movieDetail
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.TextView
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
-import com.example.movieapp.R
-import com.example.movieapp.data.local.Movie
 import com.example.movieapp.databinding.FragmentMovieDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
+class MovieDetailFragment : Fragment() {
+
+    private var _binding: FragmentMovieDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private val args: MovieDetailFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMovieDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val movie = arguments?.getParcelable<Movie>("movie")
-        view.findViewById<TextView>(R.id.titleTextView).text = movie?.title
-        view.findViewById<TextView>(R.id.overviewTextView).text = movie?.overview
-        view.findViewById<TextView>(R.id.releaseDateTextView)
-        Glide.with(this).load("https://image.tmdb.org/t/p/w500" + movie?.posterPath).into(view.findViewById(R.id.posterImageView))
+
+        val movie = args.movie
+
+        binding.titleTextView.text = movie.title
+        binding.overviewTextView.text = movie.overview
+        binding.releaseDateTextView.text = movie.release_date
+        binding.popularityRateTextView.text=movie.popularity.toString()
+        binding.usersRateTextView.text=movie.vote_average.toString()
+
+
+
+
+        Glide.with(this)
+            .load("https://image.tmdb.org/t/p/w500${movie.posterPath}")
+            .into(binding.posterImageView)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
+
